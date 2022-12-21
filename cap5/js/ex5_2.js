22b0f745dcc52b31258d10363a2106896fb1ecbe
matheusprado1 <matheusprado1096@gmail.com>
2022-12-21 13:40:15 -0300

    exemplos cap 5

---
+++ cap5/js/ex5_2.js
@@ -0,0 +1,72 @@
+//declara vetor de escopo global que irá conter os números já apostados
+let erros = [];
+
+//gera um número aleatório entre 1 e 100
+let sorteado = Math.floor(Math.random() * 100) + 1;
+
+//declara constante com o número de chances
+const CHANCES = 6;
+
+function apostarNumero() {
+  //cria referência ao campo de entrada e obtém seu conteúdo
+  let inNumero = document.getElementById("inNumero");
+  let numero = Number(inNumero.value);
+
+  //valida o número
+  if (numero <= 0 || numero > 100 || isNaN(numero)) {
+    alert("Informe um número válido...");
+    inNumero.focus();
+    return;
+  }
+
+  //referencia espaços das saídas de dados
+  let outDica = document.getElementById("outDica");
+  let outErros = document.getElementById("outErros");
+  let outChances = document.getElementById("outChances");
+
+  //se aposta do jogador for igual ao número sorteado
+  if (numero == sorteado) {
+    alert("Parabéns!! Você Acertou!!!");
+    //troca status dos botões
+    btApostar.disabled = true;
+    btJogar.className = "exibe";
+    outDica.textContent = "Parabéns!! Número sorteado: " + sorteado;
+  } else {
+    //se número existe no vetor erros
+    if (erros.indexOf(numero) >= 0) {
+      //indexOf retorna sempre maior que 0 se existir o valor no vetor
+      alert("Você já apostou o número " + numero + ". Tente outro...");
+    } else {
+      erros.push(numero); //adiciona número ao vetor
+      let numErros = erros.length; // obtém tamanho do vetor
+      let numChances = CHANCES - numErros; //calcula numero de chances
+      //exibe numero de erros, conteúdo do vetor e numero de chances
+      outErros.textContent = numErros + " (" + erros.join(", ") + ")";
+      outChances.textContent = numChances;
+      if (numChances == 0) {
+        alert("Suas chances acabaram...");
+        btApostar.disabled = true;
+        btJogar.className = "exibe";
+        outDica.textContent = "Game Over!! Número Sorteado: " + sorteado;
+      } else {
+        //usa operador ternário (condicional) para mensagem da dica
+        let dica = numero < sorteado ? "maior" : "menor";
+        outDica.textContent =
+          "Dica: Tente um número " + dica + " que " + numero;
+      }
+    }
+  }
+  //Limpa campo de entrada e posiciona cursor neste campo
+  inNumero.value = "";
+  inNumero.focus();
+}
+
+let btApostar = document.getElementById("btApostar");
+btApostar.addEventListener("click", apostarNumero);
+
+function jogarNovamente() {
+  location.reload(); //recarrega a página
+}
+
+let btJogar = document.getElementById("btJogar");
+btJogar.addEventListener("click", jogarNovamente);
